df3a93f15a8e3fddf8958ec7cfeea8cf771821ca
Jeril Sebastian <jeril.sebastian@gmail.com>
2024-04-13 18:40:42 +0530

    update formula mycli

--- my-cli.rb
+++ my-cli.rb
@@ -2,7 +2,7 @@ class MyCli < Formula
     desc "My CLI"
     homepage "https://github.com/jerilseb/my-cli"
     url "https://github.com/jerilseb/my-cli/releases/download/v1.0.1/mycli-linux-amd64.tar.gz"
-    sha256 "a25c833611457c558b949fd3ffe610fd2afb8574cbb65f2f09295560338e3a45"
+    sha256 "203e153de03778368d610aa530963ed45205a1a0d299f27ff9f6d5f747e3498d"
 
     def install
         bin.install "my-cli"
